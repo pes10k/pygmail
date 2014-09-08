70c26b661e742177393084f9c7bb2f4fa1dc52d2
Peter Snyder <snyderp@gmail.com>
2014-09-08 15:26:01 -0500

    initial packaging info

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages # Always prefer setuptools over distutils
+from codecs import open # To use a consistent encoding
+from os import path
+here = path.abspath(path.dirname(__file__))
+
+description='Tools for interacting with GMail accounts over OAuth2, optionally in the tornado event loop'
+
+setup(
+    name='pygmail',
+    version='0.5',
+    packages=['pygmail'],
+    description=description,
+    author="Peter Snyder",
+    author_email="psnyde2@uic.edu",
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='email development',
+    install_requires=['imaplib2', 'google-api-python-client']
+)
